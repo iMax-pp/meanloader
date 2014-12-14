9e066ecaa6c215fe0ed15a4610dff59f537577f6
Maxime Simon <simon.maxime@gmail.com>
2014-12-14 14:43:20 +0100

    fix(launch): send a last message to socket if no current running launches, but had running launches before

--- app/routes/launch.server.routes.js
+++ app/routes/launch.server.routes.js
@@ -6,15 +6,22 @@ module.exports = function (app) {
     app.route('/launch').get(launcher.run);
 
     app.get('socketio').on('connection', function (socket) {
-        var refresh = setInterval(function () {
-            launcher.listRunning(function (results) {
-                if (results.length !== 0) {
-                    socket.emit('running.launches', {
-                        launches: results
-                    });
-                }
-            });
-        }, 500);
+        var runningLaunches = false,
+            refresh = setInterval(function () {
+                launcher.listRunning(function (results) {
+                    if (results.length !== 0) {
+                        socket.emit('running.launches', {
+                            launches: results
+                        });
+                        runningLaunches = true;
+                    } else if (runningLaunches) {
+                        socket.emit('running.launches', {
+                            launches: results
+                        });
+                        runningLaunches = false;
+                    }
+                });
+            }, 500);
         socket.on('disconnect', function () {
             clearInterval(refresh);
         });
